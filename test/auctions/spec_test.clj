e21a3deb3d303da2e51c891ed147094d1add141d
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2023-12-14 20:38:26 +0200

    minus things

--- test/auctions/spec_test.clj
+++ test/auctions/spec_test.clj
@@ -1,37 +1,19 @@
 (ns auctions.spec-test
   (:require [auctions.spec :refer :all]
-            [clojure.spec.alpha :as s]
-            [clojure.test :refer :all]))
-(def valid-user "BuyerOrSeller|1|test@test.se")
-(def valid-auction-1 {"title" "auction"
-                       "startsAt" (java.time.LocalDateTime/parse "2023-03-15T11:50:55")
-                       "expiry" (java.time.LocalDateTime/parse "2023-03-16T11:50:55")
-                       "user" valid-user
-                       "currencyCode" "SEK"})
-(comment 
-  [:map ["title" :string] ["startsAt" :some] ["expiry" :some] ["user" :string] ["currency-code" :string]]
-  )
-(let [invalid-user "test"
-      valid-user "BuyerOrSeller|1|test@test.se"
+            [clojure.test :refer :all]
+            [malli.core :as m]))
+
+(let [valid-user "BuyerOrSeller|1|test@test.se"
       valid-auction {"title" "auction"
-                     "startsAt" (java.time.LocalDateTime/parse "2023-03-15T11:50:55")
-                     "expiry" (java.time.LocalDateTime/parse "2023-03-16T11:50:55")
+                     "startsAt" "2023-03-15T11:50:55"
+                     "expiry" "2023-03-16T11:50:55"
                      "user" valid-user
                      "currencyCode" "SEK"}
-      invalid-auction {}
-      invalid-auction2 (merge valid-auction {"user" invalid-user})
-      ]
+      invalid-auction {}]
 
   (deftest schema-spec
-    (testing "invalid user"
-      (is (= false (s/valid? :auctions/user invalid-user))))
-    (testing "valid user"
-      (is (= true (s/valid? :auctions/user valid-user))))
     
     (testing "invalid auction"
-      (is (= false (s/valid? :auctions/auction invalid-auction)))
-      (is (= false (s/valid? :auctions/auction invalid-auction2))))
+      (is (= false (m/validate auction-schema invalid-auction))))
     (testing "a valid auction"
-      (is (= true (s/valid? :auctions/auction valid-auction)))
-      (is (= valid-auction (s/conform :auctions/auction valid-auction)))
-      )))
+      (is (= true (m/validate auction-schema valid-auction))))))
